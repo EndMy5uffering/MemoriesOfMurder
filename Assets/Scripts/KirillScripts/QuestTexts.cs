8cf81d643c2ba246b2cb3a90ae23f27345fd33cd
Corallicum <kir.scherbakov2016@gmail.com>
2024-11-30 16:58:09 +0100

    Quest System

---
+++ Assets/Scripts/KirillScripts/QuestTexts.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestTexts : MonoBehaviour
+{
+    public static Quest[] quests = new Quest[1]
+    {
+        new Quest("Bring some Water to the toaster")
+    };
+}
