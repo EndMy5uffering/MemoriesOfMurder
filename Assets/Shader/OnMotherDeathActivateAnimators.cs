556bc49eab114bd44b55dd602a13946f29913b40
Pandorairq <93979100+Pandorairq@users.noreply.github.com>
2024-12-01 03:28:35 +0100

    Animator activation on Mother death discovered script

---
+++ Assets/Shader/OnMotherDeathActivateAnimators.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Animator))]
+public class OnMotherDeathActivateAnimators : MonoBehaviour
+{
+    public static UnityEvent OnMotherDeathDiscovered;
+
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnMotherDeathDiscovered.AddListener(ActivateAnimator);
+        animator = GetComponent<Animator>();
+    }
+    
+    
+    private void ActivateAnimator()
+    {
+        animator.enabled = true;
+    }
+}
